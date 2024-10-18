c1139e7d3e17bdc4055e1a38c3312c4d67c4a709
Lucas Moreira <107709625+lmoreira7@users.noreply.github.com>
2024-10-17 22:13:16 -0300

    Algoritmo Assembly - Tabela 9.asm

---
+++ Algoritmo Assembly - Tabela 9.asm
@@ -0,0 +1,20 @@
+li $t0, 8  #A
+li $t1, 3  #B
+li $t2, 2  #C
+
+xori $t3, $t0, 1 #!A
+xori $t4, $t1, 1 #!B
+xori $t5, $t2, 1 #!C
+
+and $t6, $t3, $t4  #(!A*!B)
+and $s0, $t5, $t6  #(!A*!B*!C)
+and $t7, $t3, $t1  #(!A*B)
+and $s1, $t7, $t5  #(!A*B*!C)
+and $t8, $t0, $t4  #(A*!B)
+and $s2, $t8, $t5  #(A*!B*!C)
+and $t9, $t0, $t1  #(A*B)
+and $s3, $t9, $t5  #(A*B*!C)
+
+or $s4, $s0, $s1   #(!A*!B*!C) + (!A*B*!C)
+or $s5, $s2, $s3   #(A*!B*!C) + (A*B*!C)
+or $s7, $s4, $s5   #(!A*!B*!C) + (!A*B*!C)+ (A*!B*!C) + (A*B*!C)
